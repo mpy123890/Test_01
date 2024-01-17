3a85aa2a591d3d5aa74f02d76be87887a73befe9
mpy123890 <mpy123890@gmail.com>
2024-01-17 19:56:11 +1000

    2024/1/17

--- Test_1_17/Test_1_17/Test.c
+++ Test_1_17/Test_1_17/Test.c
@@ -14,10 +14,10 @@
 
 //int main()
 //{
-//	printf("%zd\n", sizeof (bool)); //"bool" 昞帵 "_Bool"
+//	printf("%zd\n", sizeof (bool)); //"bool" 代表 "_Bool"
 //	printf("%zd\n", sizeof (long int));
 //	printf("%zd\n", sizeof (int));
-//	printf("%zd\n", sizeof (int + 1)); //error, sizeof揑昞?幃晄?峴?嶼
+//	printf("%zd\n", sizeof (int + 1)); //error, sizeof不进行计算
 //
 //		return 0;
 //
@@ -36,7 +36,7 @@
 //	printf("%d\n", a);
 //	printf("%d\n", b);
 //	printf("%d\n", b + 1); 
-//	printf("%d\n", c); //error, 嬊晹?検枹嵼錀?撪
+//	printf("%d\n", c); //error, 局部变量未在范围内
 //
 //	return 0;
 //
@@ -49,10 +49,10 @@ int main()
 	printf("%d\n", 5 - 4);
 	printf("%d\n", 2 * 5);
 	printf("%d\n", 10 / 5);
-	printf("%d\n", 3 / 2); //=1, 彍崋揑?抂擛壥惀惍悢丆?峴惍悢彍朄
-	printf("%lf\n", 3.0 / 2); //=1.5, 擛壥梫?嶼彫悢?壥丆彍崋?抂廀梫帄彮桳堦槩晜揰悢(彫悢)
+	printf("%d\n", 3 / 2); //=1, 只进行整数的除法计算
+	printf("%lf\n", 3.0 / 2); //=1.5, 需要在除号除号两边至少一边为浮点数(小数)
 	int a = 9 % 5;
-	printf("%d\n", a); //=......4, %扅擻?梡槹惍悢丆媮彍朄岪揑梋悢
+	printf("%d\n", a); //=......4, %代表除法后的余数，只能计算整数
 
 
 	return 0;
