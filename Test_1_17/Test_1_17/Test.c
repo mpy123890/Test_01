52fcdbea78e53561bfe72c23b560f2113d7554e9
mpy123890 <mpy123890@gmail.com>
2024-01-18 20:39:24 +1000

    2024/01/18

--- Test_1_17/Test_1_17/Test.c
+++ Test_1_17/Test_1_17/Test.c
@@ -42,22 +42,61 @@
 //
 //}
 
+//int main()
+//{
+//
+//	printf("%d\n", 1 + 3);
+//	printf("%d\n", 5 - 4);
+//	printf("%d\n", 2 * 5);
+//	printf("%d\n", 10 / 5);
+//	printf("%d\n", 3 / 2); //=1, 只进行整数的除法计算
+//	printf("%lf\n", 3.0 / 2); //=1.5, 需要在除号除号两边至少一边为浮点数(小数)
+//	int a = 9 % 5;
+//	printf("%d\n", a); //=......4, %代表除法后的余数，只能计算整数
+//
+//	
+//	return 0;
+//
+//}
+
+//int main()
+//{
+//    int a = 15;
+//    int b = 2;
+//    int c = a / b;
+//    printf("%d\n", c);
+//    int d = a % b;
+//    printf("%d\n", d);
+//
+//        return 0;
+//}
+
+
+//int main()
+//{
+//    int a = 15;
+//    float b = 2.0;
+//    float c = a / b;
+//    printf("%f\n", c);
+//    int d = a % b;
+//    printf("%d\n", d);
+//
+//    return 0;
+//}
+
+
 int main()
 {
-
-	printf("%d\n", 1 + 3);
-	printf("%d\n", 5 - 4);
-	printf("%d\n", 2 * 5);
-	printf("%d\n", 10 / 5);
-	printf("%d\n", 3 / 2); //=1, 只进行整数的除法计算
-	printf("%lf\n", 3.0 / 2); //=1.5, 需要在除号除号两边至少一边为浮点数(小数)
-	int a = 9 % 5;
-	printf("%d\n", a); //=......4, %代表除法后的余数，只能计算整数
-
+	int a = 5;
+	float b = 2.0;
+	printf("%f\n", a / b);
 
 	return 0;
 
+
 }
+
+
 
 
 
